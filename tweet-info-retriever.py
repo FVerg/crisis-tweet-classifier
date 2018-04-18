7871951684197c67abb8815b46051f4090b9b07c
fverg <vergonaf@gmail.com>
2018-04-18 16:07:15 +0200

    Safer key management. Added comments

--- tweet-info-retriever.py
+++ tweet-info-retriever.py
@@ -1,57 +1,52 @@
 import twython
 from twython import Twython
 import pandas as pd
+import json
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
-CONSUMER_KEY = ""  # Type your CONSUMER_KEY
-CONSUMER_SECRET = ""  # Type your CONSUMER_SECRET
-OAUTH_TOKEN = ""  # Type your OAUTH_TOKEN
-OAUTH_TOKEN_SECRET = ""  # Type your OAUTH_TOKEN_SECRET
+keys = json.load(open("keys.json"))
+
+CONSUMER_KEY = keys['consumer_key']                 # Type your CONSUMER_KEY
+CONSUMER_SECRET = keys['consumer_secret']           # Type your CONSUMER_SECRET
+OAUTH_TOKEN = keys['access_token']                 # Type your OAUTH_TOKEN
+OAUTH_TOKEN_SECRET = keys['access_token_secret']   # Type your OAUTH_TOKEN_SECRET
 
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-# tweet = twitter.show_status(id=382813437513834497)
-
-
-'''
-print("User:", tweet['user'])
-print("Text:", tweet['text'])
-'''
-
 dataset = pd.read_csv('C:/dataset/2013_pakistan_eq.csv', header=0)
 
-# dataset
-
 # Retrieving all tweet ids in order to use APIs to fetch all the informations we need on them
+
 tweet_ids = dataset["tweet_id"]
-tweet_ids.rename("ID")
 
 print(type(tweet_ids))
 
-# type(tweet_ids)
-
-# print(tweet_ids.size)
+# Removing the apexes from each Tweet ID
 
 for i in range(0, tweet_ids.size):
     tweet_ids[i] = tweet_ids[i].replace("'", "")
 
-print("Tweet ids have been correctly formatted")
+print("[DEBUG] Formatting tweets")
 
-# print(tweet_ids)
+# Create a new DataFrame, initially containing just the IDs
 
 tweet_infos = pd.DataFrame(tweet_ids)
+
+# List that will contain the new informations to add to the DataFrame tweet_infos
+
 list = []
 
+# Access to the informations of each tweet, retrieving them through Tweet ID
+# PROBLEM: Twitter allows standard user only a limited number of requests in a 15 minutes time window.
 
 for id in tweet_ids:
     try:
         tweet = twitter.show_status(id=id)
     except twython.exceptions.TwythonError as e:
-        print(e)
-        pass
+        print(e)    # If an exception occurs (APIs return an unexpected HTTP response code) we print it
+        pass        # Exception is not handled yet
     else:
         list.append(tweet['id_str'])
- #    tweet_infos.append(tweet['id_str'])
 
 print(list)
