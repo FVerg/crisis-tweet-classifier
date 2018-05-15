816a6525df984081fa9f9e4f22f1c1ccd34a3f82
fverg <vergonaf@gmail.com>
2018-05-15 20:11:18 +0200

    Added script to select only irrelevant tweets

---
+++ irrelevant_tweets.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+dataset = pd.read_csv("C:/dataset/2013_pakistan_eq_metadata.csv", header=0)
+
+relevant_labels = ("caution_and_advice", "displaced_people_and_evacuations", "infrastructure_and_utilities_damage",
+                   "injured_or_dead_people", "missing_trapped_or_found_people", "other_useful_information")
+
+dataset = dataset[~dataset["Label"].isin(relevant_labels)]
+
+dataset = dataset.set_index("TweetID")
+
+col_names = list(dataset.columns.values)
+
+dataset.to_csv("C:/dataset/2013_pakistan_eq_metadata_irrelevant.csv",
+               header=col_names, index=True, sep=',', mode='w')
