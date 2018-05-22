6075a005ccfca74c5465ecc207777aa36f32470c
fverg <vergonaf@gmail.com>
2018-05-22 17:00:40 +0200

    Comments

--- entities_extraction.py
+++ entities_extraction.py
@@ -1,3 +1,8 @@
+# In this code snippet I would like to take the original dataset with original tweet texts,
+# and create a new dataset containing for each tweet, the 0/1 information about the presence or
+# absence of each tag available in NLP Tagger Tagset.
+# The tagset is in tagset.txt
+
 from nltk.tag.stanford import CoreNLPNERTagger
 from utilities import tweets_to_csv, get_tagset
 import pandas as pd
