e8b621ab2d50f4b64f1d087c646225b21ecdb739
fverg <vergonaf@gmail.com>
2018-05-23 08:49:53 +0200

    Trying to create dictionary with keys from a list. Still not working.

--- entities_extraction.py
+++ entities_extraction.py
@@ -5,11 +5,33 @@
 
 from nltk.tag.stanford import CoreNLPNERTagger
 from utilities import tweets_to_csv, get_tagset
+
 import pandas as pd
 
 dataset = pd.read_csv('C:/dataset/2014_california_eq.csv', header=0)
 
 # List containing all the possible tags
 tagset = get_tagset("tagset.txt")
+for i in range(len(tagset)):
+    tagset[i] = tagset[i].lower().capitalize()
 
 dataset = pd.read_csv('C:/dataset/2014_california_eq.csv', header=0)
+
+tweets = []
+
+tweet_ids = dataset["tweet_id"]
+tweet_text = dataset["tweet_text"]
+'''
+attributes = ["TweetID", "Text"]
+attributes.extend(tagset)
+print(attributes)
+'''
+for id, text in zip(tweet_ids, tweet_text):
+    filtered_text = ""
+    for word in text:
+        if word.startswith("#"):
+            filtered_text = filtered_text + word.replace("#", "")
+        else:
+            filtered_text = filtered_text + word
+    # Don't know how to create dictionary using some keys taken from list
+    tweets.append(dict([("TweetID", id), ("Text", text)], [tag, []] for tag in tagset))
